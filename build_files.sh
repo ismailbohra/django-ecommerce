adeeda53cd606114e2669494ff6773457d497322
ismailbohra <ismailbohra99@gmail.com>
2024-06-24 04:46:38 +0530

    vecel

--- build_files.sh
+++ build_files.sh
@@ -6,6 +6,6 @@ mkdir -p staticfiles_build
 python3.9 -m pip install -r requirements.txt
 
 # Collect static files
-python3.11 manage.py collectstatic --noinput --clear
+python3.9 manage.py collectstatic --noinput --clear
 
 echo "BUILD END"
