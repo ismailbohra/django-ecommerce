8e3d9a8b6560e5c7c6c358bf73d538df6acebbea
ismailbohra <97942595+ismailbohra@users.noreply.github.com>
2024-06-25 01:11:06 +0530

    test

--- ecommerce/build_files.sh
+++ ecommerce/build_files.sh
@@ -6,9 +6,9 @@ echo "BUILD START"
 mkdir -p staticfiles_build
 
 # Install Python dependencies from requirements.txt
-python3.9 -m pip install -r requirements.txt
+python3.11 -m pip install -r requirements.txt
 
 # Collect static files
-python3.9 manage.py collectstatic --noinput --clear --directory staticfiles_build
+python3.11 manage.py collectstatic --noinput --clear --directory staticfiles_build
 
 echo "BUILD END"
