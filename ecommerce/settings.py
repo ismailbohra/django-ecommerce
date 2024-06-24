d9e8014accdb3d2eef570c0067aa5274cfe5ac42
ismailbohra <97942595+ismailbohra@users.noreply.github.com>
2024-06-25 02:05:13 +0530

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','django-ecommerce-4fpa.onrender.com']
 
 
 # Application definition
