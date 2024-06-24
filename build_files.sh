46e910a1dc3574c821f0b3b63393c21ed0511e85
ismailbohra <97942595+ismailbohra@users.noreply.github.com>
2024-06-25 01:34:36 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -3,7 +3,7 @@
 echo "BUILD START"
 
 # Install Python dependencies from requirements.txt
-pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
 # Collect static files
 python3 manage.py collectstatic
