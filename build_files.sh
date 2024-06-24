671b9874390a5e6a51f1389855f6f4007b7ae104
ismailbohra <97942595+ismailbohra@users.noreply.github.com>
2024-06-25 01:33:38 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -2,13 +2,10 @@
 
 echo "BUILD START"
 
-# Ensure staticfiles_build directory exists
-mkdir -p staticfiles_build
-
 # Install Python dependencies from requirements.txt
-python3 -m pip install -r requirements.txt
+pip install -r requirements.txt
 
 # Collect static files
-python3 manage.py collectstatic --noinput --clear --directory staticfiles_build
+python3 manage.py collectstatic
 
 echo "BUILD END"
